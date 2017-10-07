205029152aebd82f8726293926ee1eb95aef8b42
palash1 <wbpalash@gmail.com>
2017-10-07 09:52:08 +0530

    ewrere
    erew

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,7 +7,7 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  public color:string="blue";
+  public color:string="blue11";
   constructor(public navCtrl: NavController) {
 
   }
